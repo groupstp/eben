1bccae4f9622638dccca03d776dabcdc254bf3f3
r.yurgin <yurgin.rodion@gmail.com>
2018-02-14 11:25:22 +0800

    add midels and add some views

--- eben/rating/models.py
+++ eben/rating/models.py
@@ -2,4 +2,23 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-# Create your models here.
+class AllRating(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    all_rating = models.IntegerField
+
+
+class CategoryRating(models.Model):
+    category = models.CharField
+
+
+class Rating(models.Model):
+    rating = models.ForeignKey(AllRating, on_delete=models.CASCADE)
+    category = models.ForeignKey(CategoryRating, on_delete=models.CASCADE)
+    current_rating = models.IntegerField
+
+
+class HistoryRating(models.Model):
+    rating = models.ForeignKey(Rating, on_delete=models.CASCADE)
+    action = models.CharField
+    value = models.IntegerField(editable=False)
+    date_time = models.DateTimeField(auto_now_add=True)
--- eben/rating/views.py
+++ eben/rating/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import DetailView, ListView
+from django.utils.translation import ugettext as _
+
+from .models import AllRating, Rating
 
 
-# Create your views here.
+class AllRatingDetail(DetailView):
+    model = AllRating
+    slug_field = 'user.username'
+    slug_url_kwarg = 'user.username'
+
+
+class RatingList(ListView):
+    model = Rating
+    slug_field = 'rating' + AllRatingDetail.slug_field
+    slug_url_kwarg = 'rating' + AllRatingDetail.slug_url_kwarg
+
+
+class RatingDetail(DetailView):
+    model = Rating
+    slug_field = 'rating' + AllRatingDetail.slug_field
+    slug_url_kwarg = 'rating' + AllRatingDetail.slug_url_kwarg
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        username = self.kwargs.get(self.slug_url_kwarg)
+        category = self.kwargs.get(self.slug_url_kwarg)
+        if category is not None:
+            queryset = queryset.filter(**{category: category})
+        if username is not None:
+            queryset = queryset.filter(**{username: username})
+        try:
+            obj = queryset.get()
+        except queryset.model.DoesNotExist:
+            raise Http404(_("No %(verbose_name)s found matching the query") %
+                          {'verbose_name': queryset.model._meta.verbose_name})
+        return obj

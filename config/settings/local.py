bcabd80f66852748ae9c2a73074088eb0c8cab3a
Egor Livshits <rjaxkuznxkra@gmail.com>
2018-02-16 20:08:33 +0800

    profile-ui and form for editing

--- config/settings/local.py
+++ config/settings/local.py
@@ -44,7 +44,7 @@ CACHES = {
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 INSTALLED_APPS += ['debug_toolbar', ]
 
-INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', 'localhost', '[::1]', '0.0.0.0']
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', 'localhost', '[::1]', '0.0.0.0', '192.168.99.100', '192.168.99.1', '*']
 ALLOWED_HOSTS = INTERNAL_IPS
 
 

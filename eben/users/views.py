15ceef5e7bcd615adf571bfd4c5b7e0e0f2598c8
r.yurgin <yurgin.rodion@gmail.com>
2018-02-28 16:03:07 +0800

    Bad and ugly Ajax Kostil'

--- eben/users/views.py
+++ eben/users/views.py
@@ -1,9 +1,36 @@
+import json
+
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseBadRequest, HttpResponse
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import User
+from django.views.generic import FormView
+
+class AjaxFormMixin(FormView):
+
+    template_name = 'form_ajax.html'
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponse('OK')
+
+    def form_invalid(self, form):
+        errors_dict = json.dumps(dict([(k, [e for e in v]) for k, v in form.errors.items()]))
+        return HttpResponseBadRequest(json.dumps(errors_dict))
+
+
+class AjaxUpdateFormMixin(AjaxFormMixin, UpdateView):
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(AjaxFormMixin, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(AjaxFormMixin, self).post(request, *args, **kwargs)
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
@@ -22,12 +49,14 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
                        )
 
 
-class UserUpdateView(LoginRequiredMixin, UpdateView):
+class UserUpdateView(LoginRequiredMixin, AjaxUpdateFormMixin):
 
     fields = ['name', 'first_name', 'last_name', 'avatar', 'date_of_birth', 'info', ]
 
     # we already imported User in the view code above, remember?
     model = User
+
+    template_name = 'users/user_form.html'
 
     # send the user back to their own page after a successful update
     def get_success_url(self):

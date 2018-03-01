aef9a4b988af78835c9bbf6bd2521b0b7d5555f5
r.yurgin <yurgin.rodion@gmail.com>
2018-03-01 19:36:03 +0800

    fix rest api for users

--- eben/users/urls.py
+++ eben/users/urls.py
@@ -1,12 +1,22 @@
-from rest_framework import routers
+from django.conf.urls import url
 
 from . import views
 
 
-router = routers.SimpleRouter()
-router.register(r'', views.UserViewSet, 'users')
-router.register(r'user', views.UserView, 'user')
-router.register(r'update', views.UserUpdateView, 'update')
-
-
-urlpatterns = router.urls
+urlpatterns = [
+    url(
+        regex=r'^$',
+        view=views.UserViewList.as_view(),
+        name='users'
+    ),
+    url(
+        regex=r'^(?P<username>[a-zA-Z0-9]+)/$',
+        view=views.UserView.as_view(),
+        name='user'
+    ),
+    url(
+        regex=r'^update/(?P<username>.+)/$',
+        view=views.UserUpdateView.as_view(),
+        name='update'
+    )
+]
--- eben/users/views.py
+++ eben/users/views.py
@@ -1,30 +1,50 @@
-from rest_framework import viewsets, mixins
+from django.shortcuts import redirect
+from rest_framework import generics
+from rest_framework.renderers import JSONRenderer, MultiPartRenderer, TemplateHTMLRenderer
+from rest_framework.response import Response
 
 from .serializers import UserSerializer, UserUpdateSerializer
 from .models import User
 
 
-class UserView(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+class UserView(generics.RetrieveAPIView):
     """
     Получение данных пользователя
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    renderer_classes = [JSONRenderer, ]
     lookup_field = 'username'
 
 
-class UserUpdateView(mixins.UpdateModelMixin, viewsets.GenericViewSet):
-    """
-    Редактирование пользователя
-    """
-    queryset = User.objects.all()
-    serializer_class = UserUpdateSerializer
-    lookup_field = 'username'
-
-
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewList(generics.ListAPIView):
     """
     Получение списка пользователей
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    renderer_classes = [JSONRenderer, ]
+
+
+class UserUpdateView(generics.RetrieveUpdateAPIView):
+    """
+    Редактирование пользовательской информации
+    """
+    renderer_classes = [TemplateHTMLRenderer, ]
+    template_name = 'users/user_form.html'
+    queryset = User.objects.all()
+    lookup_field = 'username'
+    serializer_class = UserUpdateSerializer
+
+    def retrieve(self, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.serializer_class(instance)
+        return Response({'serializer': serializer, type(instance).__name__: instance})
+
+    def post(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.serializer_class(instance, data=request.data)
+        if not serializer.is_valid():
+            return Response({'serializer': serializer, type(instance).__name__: instance})
+        serializer.save()
+        return redirect('home')

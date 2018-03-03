30b50baa9ec91c65f88c5dd254ad288a4ef44475
r.yurgin <yurgin.rodion@gmail.com>
2018-03-03 23:12:53 +0800

    fix redirect login problem

--- config/settings/base.py
+++ config/settings/base.py
@@ -265,7 +265,7 @@ SOCIALACCOUNT_ADAPTER = 'eben.users.adapters.SocialAccountAdapter'
 # Custom user app defaults
 # Select the correct user model
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = 'users:redirect'
+LOGIN_REDIRECT_URL = 'ui'
 LOGIN_URL = 'account_login'
 
 # SLUGLIFIER
